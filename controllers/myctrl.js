68cd6c99fe1ed81f0907e4d2db43b82ecf82690c
Bill Hoang <billhoang79@gmail.com>
2017-02-07 13:21:26 -0800

    fixed some camel casing

--- controllers/myctrl.js
+++ controllers/myctrl.js
@@ -1,17 +1,17 @@
 app.controller('myCtrl', function($scope, myFactory, Auth) {
-	console.log(myFactory.getdata())
-	console.log(myFactory.mydata)
+	console.log(myFactory.getData())
+	console.log(myFactory.myData)
 })
 
 // Factory has to return a value
 // Use to bundle a bunch of functionality
 // Uses: wrappers for APIs, Authentications 
 app.factory('myFactory', function) {
-	var mydata = "this is some other data"
-	var myfunc = function() {} 
+	var myData = "this is some other data"
+	var myFunc = function() {} 
 	return {
-		getdata: function() {
-			return this.mydata
+		getData: function() {
+			return this.myData
 		}
 	}
 })
